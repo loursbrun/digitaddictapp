785c26253c9a7d4b0a8c7381abc773f6e2e0dbf8
Fabien Brun <fabienbrun@MacBook-Pro-de-Fabien.local>
2021-04-22 12:09:56 +0200

    add frontend

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+  //uri: 'http://moviemaker.eu-4.evennode.com:4000/graphql'
+});
+
+ReactDOM.render(
+  <Router>
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
