e247e2a2a7cebe75c870f71865fc54d40bfb6c51
Fabien Brun <fabienbrun@MacBook-Pro-de-Fabien.local>
2021-04-22 12:37:50 +0200

    fix app test

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks'
-import {BrowserRouter as Router} from 'react-router-dom'
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-  //uri: 'http://moviemaker.eu-4.evennode.com:4000/graphql'
-});
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </React.StrictMode>
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
